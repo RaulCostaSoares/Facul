21d4783b3e7f12813193efa04e85c876d16855a0
Raul Costa <raul.soares002@edu.pucrs.br>
2024-04-02 18:39:09 -0300

    exercicio 27

--- Entrega3/Exercicio027.c
+++ Entrega3/Exercicio027.c
@@ -2,7 +2,22 @@
 
 int main(){
 
+    double sal, aum, salF;
 
+    scanf("%lf", &sal);
+
+    if(sal<=350.0){
+        aum = 15.0;
+    } else if(sal>350.0 && sal<600.0){
+        aum = 10.0;
+    } else if(sal>=600.0 && sal<=900.0){
+        aum = 5.0;
+    }
+
+    salF = sal + (sal*(aum/100));
+
+    printf("%.4lf\n", aum);
+    printf("%.4lf", salF);
 
 
     return 0;
