f68458b94639ee0ba1f3f623f9e0592292cf3ff7
raul.costa <raul.costa@travicar.com.br>
2024-08-14 18:20:07 -0300

    n

--- POO/hello.cpp
+++ POO/hello.cpp
@@ -6,17 +6,21 @@ using namespace std;
 
 int main(){
     
-    string nome;
-
-    double A, B, C;
+    double A, B, C, RAIO, TRIANGULO, TRAPEZIO, QUADRADO, RETANGULO;
     
-    cin >> nome;
-    cin >> A;
-    cin >> B;
-    
-    C = A+(0.15*B);
+    cin >> A >> B >> C;
 
-    cout << "TOTAL = R$ "<<fixed<<setprecision(2) << C <<endl;
-         
+    TRIANGULO = (A*C)/2;
+    RAIO = (3.14159*(C*C));
+    TRAPEZIO = ((A+B)/2)*C;
+    QUADRADO = B*B;
+    RETANGULO = A*B;
+
+    cout << "TRIANGULO: "<<fixed<<setprecision(3) << TRIANGULO <<endl;
+    cout << "CIRCULO: "<<fixed<<setprecision(3) << RAIO <<endl;
+    cout << "TRAPEZIO: "<<fixed<<setprecision(3) << TRAPEZIO <<endl;
+    cout << "QUADRADO: "<<fixed<<setprecision(3) << QUADRADO <<endl;
+    cout << "RETANGULO: "<<fixed<<setprecision(3) << RETANGULO <<endl;
+    
     return 0;
 }
